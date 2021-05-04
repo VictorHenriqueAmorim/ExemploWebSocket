15365a0bf5e1f49f27a9a42898297a34d62f8247
Vito <victoramorim895623@gmail.com>
2021-05-03 22:50:47 -0300

    Add example

---
+++ Startup.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.WebSockets;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using WebSocketServer.Middleware;
+
+namespace WebSocketServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddWebSocketManager();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+           app.UseWebSockets();
+           app.UseWebSocketServer();
+           app.Run(async context => {
+               Console.WriteLine("Hello from the 3rd request delegate.");
+               await context.Response.WriteAsync("Hello from the 3rd request delegate.");
+           });
+        }
+    }
+}
